2d54cf955b7707b35135b25b2170013a8dc5bd5c
TheAnkushAgarwal <ankush.agarwal.202@gmail.com>
2023-10-24 23:53:33 +0530

    travel list app created.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Logo from './Logo.js';
+import Form from './Form.js';
+import PackingList from './PackingList.js';
+import Stats from './Stats.js';
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 4, packed: true },
+];
+
+
+
+export default function App() {
+  const[items,setItems] = useState(initialItems);
+  
+
+  function handleAddItems(item){
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id){
+    console.log(id);
+    setItems((items)=>items.filter((item)=> item.id !== id))
+  }
+
+  function handleToggleItem(id){
+    setItems(items => items.map(item => item.id === id ? {...item, packed: !item.packed}: item))
+  }
+
+  function handleClearList(){
+    const confirmed = window.confirm("Are you sure you want to delete all items?");
+
+    if(confirmed)
+      setItems([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo/>
+      <Form onAddItems = {handleAddItems}/>
+      <PackingList item= {items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearList = {handleClearList}/>
+      <Stats items={items} />
     </div>
-  );
-}
 
-export default App;
+  );
+
+}
---
+++ src/PackingList.js
@@ -0,0 +1,35 @@
+import {useState} from 'react';
+import Item from './Item.js';
+
+export default function PackingList({item, onDeleteItem, onToggleItem, onClearList}){
+    const [sortBy, setSortBy ] = useState("input");
+  
+    let sortedItems;
+  
+    if(sortBy === 'input') sortedItems = item;
+  
+    if(sortBy === 'description')
+      sortedItems = item.slice().sort((a,b)=> a.description.localeCompare(b.description));
+  
+    if(sortBy === 'packed')
+      sortedItems = item.slice().sort((a,b) => Number(a.packed)- Number(b.packed));
+  
+  
+    return (
+      <div className="list">
+        <ul className="list"> 
+        {sortedItems.map((item) => <Item item = {item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id}/>)}
+        </ul>
+  
+        <div className="actions">
+          <select value= {sortBy} onChange={(e)=> setSortBy(e.target.value)}>
+            <option value = "input"> Sort by input order</option>
+            <option value="description"> Sort by description</option>
+            <option value="packed">Sort by packed status</option>
+          </select>
+  
+          <button onClick ={onClearList} >Clear List</button>
+        </div>
+      </div>
+    )
+  }
